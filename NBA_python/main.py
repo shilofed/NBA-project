08ce38adf93d3a7ef16774e01bb22cba0b6674e1
moshBengi <71219312+moshBengi@users.noreply.github.com>
2021-12-18 21:55:28 +0200

    Add files via upload
    some score

--- NBA_python/main.py
+++ NBA_python/main.py
@@ -1,4 +1,18 @@
 from games_stats_factory import *
+import numpy as np
 
 if __name__ == '__main__':
-    get_games_stats()
+    games_stats = get_games_stats()
+    biggest_score = -np.inf
+    best_score = None
+    for game in games_stats:
+        score = game.get_score()
+        print(game.other_stats.TEAM_CITY[0] + " vs " + game.other_stats.TEAM_CITY[1] + " score: " + str(score))
+
+        if score > biggest_score:
+            biggest_score = score
+            best_score = game
+    if best_score == None:
+        print("no game last night")
+    else:
+        print(best_score.other_stats.TEAM_CITY[0] + " vs " + best_score.other_stats.TEAM_CITY[1])
