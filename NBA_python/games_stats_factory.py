8ec5d56eb1c129d30ea21afb93eebbd2a95b21b1
shilofed <shilofadida07@gmail.com>
2021-12-27 12:36:44 +0200

    added player score

--- NBA_python/games_stats_factory.py
+++ NBA_python/games_stats_factory.py
@@ -1,4 +1,4 @@
-from nba_api.stats.endpoints import scoreboardv2, boxscoresummaryv2, boxscoreadvancedv2, BoxScorePlayerTrackV2
+from nba_api.stats.endpoints import scoreboardv2, boxscoresummaryv2, BoxScoreTraditionalV2
 from nba_api.stats import endpoints
 from tabulate import tabulate
 from datetime import datetime, timedelta
@@ -18,7 +18,7 @@ def get_games_ids(games_date=get_yesterday()):
     :param games_date: date of the required games
     :return: list with the game_ids of the games that day
     """
-    games = scoreboardv2.ScoreboardV2(game_date=get_yesterday())
+    games = scoreboardv2.ScoreboardV2(game_date=games_date)
     games_df = games.game_header.get_data_frame()
     games_df = games_df[games_df["GAME_STATUS_ID"] == 3]  # only finished games
     games_ids = [game_id for game_id in games_df["GAME_ID"]]
@@ -36,6 +36,8 @@ def get_games_stats(games_date=get_yesterday()):
         game_stats = boxscoresummaryv2.BoxScoreSummaryV2(game_id=game_id)
         line_score = game_stats.line_score.get_data_frame()
         other_stats = game_stats.other_stats.get_data_frame()
+        player_stats = BoxScoreTraditionalV2(game_id=game_id).player_stats.get_data_frame()
+
 
         # box_score = boxscoreadvancedv2.BoxScoreAdvancedV2(game_id=game_id)
         # first_team_box = box_score.data_sets[0].get_data_frame()
