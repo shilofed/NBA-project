2cd150a77aaeeeb84a7b39de83f28f4015351938
moshBengi <moshbengi@gmail.com>
2022-03-08 14:48:10 +0200

    I feel lucky button

--- NBA_Android_Studio/app/src/main/java/com/example/a2_screens/Preference.kt
+++ NBA_Android_Studio/app/src/main/java/com/example/a2_screens/Preference.kt
@@ -17,10 +17,16 @@ class Preference : AppCompatActivity() {
         val prefArray = resources.getStringArray(R.array.types_of_game)
         val prefMap = prefArray.map { it to 5 }.toMap().toMutableMap()
         val prefButton: Button = findViewById(R.id.back_button)
+        val luckyButton: Button = findViewById(R.id.lucky_button)
         prefButton.setOnClickListener{
 //            prefButton.addExtraDataToAccessibilityNodeInfo(prefMap)
             go_back(prefButton, prefMap)
         }
+        luckyButton.setOnClickListener{
+//            prefButton.addExtraDataToAccessibilityNodeInfo(prefMap)
+            I_feel_lucky(prefButton, prefMap)
+        }
+
 
         val seek0: SeekBar = findViewById(R.id.seekBar0)
         seek0.setOnSeekBarChangeListener(object :
@@ -102,30 +108,40 @@ class Preference : AppCompatActivity() {
                 prefMap[prefArray[3]] = seek.progress
             }
         })
-
-        val seek4: SeekBar = findViewById(R.id.seekBar4)
-        seek4.setOnSeekBarChangeListener(object :
-            SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seek: SeekBar,
-                                           progress: Int, fromUser: Boolean) {
-//                prefMap[prefArray[0]] = progress
-
-            }
-
-            override fun onStartTrackingTouch(seek: SeekBar) {
-
-                // write custom code for progress is started
-            }
-
-            override fun onStopTrackingTouch(seek: SeekBar) {
-
-                prefMap[prefArray[4]] = seek.progress
-            }
-        })
+//
+//        val seek4: SeekBar = findViewById(R.id.seekBar4)
+//        seek4.setOnSeekBarChangeListener(object :
+//            SeekBar.OnSeekBarChangeListener {
+//            override fun onProgressChanged(seek: SeekBar,
+//                                           progress: Int, fromUser: Boolean) {
+////                prefMap[prefArray[0]] = progress
+//
+//            }
+//
+//            override fun onStartTrackingTouch(seek: SeekBar) {
+//
+//                // write custom code for progress is started
+//            }
+//
+//            override fun onStopTrackingTouch(seek: SeekBar) {
+//
+//                prefMap[prefArray[4]] = seek.progress
+//            }
+//        })
 
 
     }
+    fun I_feel_lucky(view: View, prefMap: Map<String, Int>) {
+        val intent = Intent(this, MainActivity::class.java)
+        for ((key, value) in prefMap){
+            intent.putExtra(key, 5)
+            print(key)
+            print(value)
+        }
 
+
+        startActivity(intent)
+    }
 
     fun go_back(view: View, prefMap: Map<String, Int>) {
         val intent = Intent(this, MainActivity::class.java)
