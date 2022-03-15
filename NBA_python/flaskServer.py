ced8c583608501fcecd328c35ebe62b70e9b163e
moshBengi <moshbengi@gmail.com>
2022-03-15 18:33:58 +0200

    add: check close game in the game itself remove: pick for me bar (there is a button already) change: the flask server pull the data one time and not in every call now the request very fast, work on emulator and phone

--- NBA_python/flaskServer.py
+++ NBA_python/flaskServer.py
@@ -8,11 +8,11 @@ import ast
 
 app = Flask(__name__)
 
-
+games_stats = games_stats_factory.get_games_stats()
 @app.route("/<arg>")
 def get_best_game(arg):
     print(arg)
-    games_stats = games_stats_factory.get_games_stats()
+
     biggest_score = -np.inf
     pref = arg.split(";")
     pref = list(filter(bool, pref))
@@ -26,15 +26,20 @@ def get_best_game(arg):
         score = 0
         if 'Great comeback' in pref_dict:
             score += int(pref_dict['Great comeback']) * game.get_score_greatest_comeback()
+            # print("great comback ", score)
         if 'Close game' in pref_dict:
             score += int(pref_dict['Close game']) * game.get_score_close_game()
+            # print("close game", int(pref_dict['Close game']) * game.get_score_close_game())
         if 'Good teams' in pref_dict:
             score += int(pref_dict['Good teams']) * game.get_score_best_teams()
-        if 'Pick for me' in pref_dict:
-            score += int(pref_dict['Pick for me']) * game.get_score()
+            # print("good teams", int(pref_dict['Good teams']) * game.get_score_best_teams())
+        # if 'Pick for me' in pref_dict:
+        #     score += int(pref_dict['Pick for me']) * game.get_score()
         if 'personal performance' in pref_dict:
             score += int(pref_dict['personal performance']) * game.get_score_personal_performance()
+            # print("personal performance", int(pref_dict['personal performance']) * game.get_score_personal_performance())
 
+        # print("score= ", score)
         if score > biggest_score:
             biggest_score = score
             best_score = game
@@ -45,9 +50,11 @@ def get_best_game(arg):
         json_response["response_tag"] = 1
         json_response["team_1_id"] = int(best_score.line_score.TEAM_ID[0])
         json_response["team_2_id"] = int(best_score.line_score.TEAM_ID[1])
-
+    # print("biggest score = ", biggest_score)
+    print("return")
     return json.dumps(json_response)
 
 
 if __name__ == "__main__":
+    # app.debug = True
     app.run(host='0.0.0.0')  # initialize server
